8f666287e64345e74ea8fda3ed8dfbc62ab5895d
sam <sampurna.chapagainnn@gmail.com>
2022-06-29 12:56:48 +0100

    add some data to data.js file

---
+++ server/data.js
@@ -0,0 +1,52 @@
+const record = [
+    {
+        url: 'https://www.imdb.com/chart/top/',
+        title: 'IMDb Top 250 Movies',
+        discription: 'MDb Top 250 Movies · 1. The Shawshank Redemption (1994) · 2. The Godfather (1972) · 3. The Dark Knight (2008) · 4. The Godfather Part II (1974) · 5. 12 Angry Men ( ...'
+    },
+    {
+        url: 'https://editorial.rottentomatoes.com/guide/popular-movies/',
+        title: '30 Most Popular Movies Right Now - Rotten Tomatoes',
+        discription: '30 Most Popular Movies Right Now · Elvis (2022) 78% · The Black Phone (2021) 84% · Lightyear (2022) 75% · Jurassic World Dominion (2022) 31% · Top Gun: Maverick ( ...'
+    },
+    {
+        url: 'https://www.marieclaire.com/culture/g2509/movies-to-watch-before-30/',
+        title: '90 Must-Watch Movies: The Best Movies of All Time',
+        discription: '6 May 2022 — The all-time best movies to watch, from romance and comedy to drama and every genre in between.'
+    },
+    {
+        url: 'https://www.youtube.com/feed/storefront',
+        title: 'Movies & Shows - YouTube',
+        discription: 'Find the latest and greatest movies and shows all available on YouTube.com/movies. From award-winning hits to independent releases, watch on any device and ...'
+    },
+    {
+        url: 'https://stacker.com/stories/1587/100-best-movies-all-time',
+        title: '100 Best Movies of All Time | Stacker',
+        discription: '31 May 2022 — Only English-language movies released in the U.S. were considered for the list. Additionally, each movie needed at least 20,000 votes on IMDb, ...'
+    },
+    {
+        url: 'https://www.commonsensemedia.org/lists/50-movies-all-kids-should-watch-before-theyre-12',
+        title: '50 Movies All Kids Should Watch Before They are 12',
+        discription: 'Common Sense Media editors help you choose 50 Movies All Kids Should Watch Before They are 12. Must-see movie night picks packed with great messages and role ...'
+    },
+    {
+        url: 'https://www.reddit.com/r/movies/',
+        title: 'Movie News and Discussion - Reddit',
+        discription: 'The goal of /r/Movies is to provide an inclusive place for discussions and news about films with maj… More. Movie. 28.3M members • 9.4K online.'
+    },
+    {
+        url: 'https://www.cnet.com/culture/entertainment/netflix-the-43-absolute-best-films-to-watch-now/',
+        title: 'Netflix: The 43 Absolute Best Movies to Watch - CNET',
+        discription: '2 days ago — To cut time, you will find highlights of new content below, as well as CNET full list of best movie originals on Netflix. What is new this week ( ...'
+    },
+    {
+        url: 'https://www.youtube.com/channel/UClgRkhTL3_hImCAmdLfDE4g',
+        title: 'Movies and Shows - YouTube',
+        discription: 'Find the latest and greatest movies and shows all available on YouTube.com/movies. From award-winning hits to independent releases, watch on any device and ...'
+    },
+    {
+        url: 'https://www.tomsguide.com/news/7-best-new-movies-to-stream-this-week-on-netflix-hbo-max-hulu-and-more-june-28',
+        title: '7 best new movies to stream this week on Netflix, HBO Max ...',
+        discription: '1 day ago — The seven top new movies you can stream at home this week include Edgar Wright latest, a big animated feature and an important ...'
+    }
+]
